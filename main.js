ab01ae9502dc384ef23c07e93f0422aad58370e5
Rasmus Taulborg Hummelmose <rth@wallmob.com>
2016-03-15 19:32:19 +0100

    Added proxy for Plex

--- main.js
+++ main.js
@@ -10,14 +10,18 @@ var ssl = {
     cert: certificate
 };
 
-var proxy_static_files = new httpProxy.createProxyServer({
+var proxyStaticFiles = new httpProxy.createProxyServer({
     ssl: ssl,
     secure: true,
     target: 'https://local.rasmusth.dk:20900'
 });
 
-var proxy_transmission = new httpProxy.createProxyServer({
+var proxyTransmission = new httpProxy.createProxyServer({
     target: 'http://192.168.1.123:9091'
+});
+
+var proxyPlex = new httpProxy.createProxyServer({
+    target: 'http://localhost:32400'
 });
 
 var handler = function (req, res) {
@@ -28,11 +32,14 @@ var handler = function (req, res) {
         res.end('Oops, something went very wrong...');
     };
     if (req.headers.host === 'static-files.rasmusth.dk') {
-        proxy_static_files.proxyRequest(req, res);
-        proxy_static_files.on('error', errorHandler);
+        proxyStaticFiles.proxyRequest(req, res);
+        proxyStaticFiles.on('error', errorHandler);
     } else if (req.headers.host === 'transmission.rasmusth.dk') {
-        proxy_transmission.proxyRequest(req, res);
-        proxy_transmission.on('error', errorHandler);
+        proxyTransmission.proxyRequest(req, res);
+        proxyTransmission.on('error', errorHandler);
+    } else if (req.headers.host === 'plex.rasmusth.dk') {
+        proxyPlex.proxyRequest(req, res);
+        proxyPlex.on('error', errorHandler());
     }
 };
 
