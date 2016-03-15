db9ac3e1230d4597c646357f3b5306a73523f4e0
Rasmus Taulborg Hummelmose <rth@wallmob.com>
2016-03-15 19:19:34 +0100

    Added proxy for transmission

--- main.js
+++ main.js
@@ -16,15 +16,23 @@ var proxy_static_files = new httpProxy.createProxyServer({
     target: 'https://local.rasmusth.dk:20900'
 });
 
+var proxy_transmission = new httpProxy.createProxyServer({
+    target: 'http://192.168.1.123:9091'
+});
+
 var handler = function (req, res) {
     console.log('Host: ' + req.headers.host);
+    var errorHandler = function(err, req, res) {
+        if (err) console.log(err);
+        res.writeHead(500);
+        res.end('Oops, something went very wrong...');
+    };
     if (req.headers.host === 'static-files.rasmusth.dk') {
         proxy_static_files.proxyRequest(req, res);
-        proxy_static_files.on('error', function(err, req, res) {
-            if (err) console.log(err);
-            res.writeHead(500);
-            res.end('Oops, something went very wrong on static-files...');
-        });
+        proxy_static_files.on('error', errorHandler);
+    } else if (req.headers.host === 'transmission.rasmusth.dk') {
+        proxy_transmission.proxyRequest(req, res);
+        proxy_transmission.on('error', errorHandler);
     }
 };
 
